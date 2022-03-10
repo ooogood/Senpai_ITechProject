940e67328d1fb2de0993202040199ef5d5b7cf5b
JosephChen1993 <ooo93301@gmail.com>
2022-03-10 15:36:08 +0000

    upload first test

--- senpai/tests.py
+++ senpai/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from senpai.models import UserProfile, Module, Note, Enrollment, Comment, Like
 
 # Create your tests here.
+class ModuleModelTests(TestCase):
+	def test_slug(self):
+		mod = Module(name="Testing Module")
+		mod.save()
+		self.assertEqual(mod.slug, "testing-module")
+		self.assertEqual(mod.id, 1)
