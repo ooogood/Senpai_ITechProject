5e069e20e9c5502e26f2e0a7f4718687c95015c7
JosephChen1993 <ooo93301@gmail.com>
2022-03-11 13:35:24 +0000

    classify module.html, seperate main func base

--- senpai/tests.py
+++ senpai/tests.py
@@ -1,10 +1,22 @@
 from django.test import TestCase
 from senpai.models import UserProfile, Module, Note, Enrollment, Comment, Like
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class ModuleModelTests(TestCase):
-	def test_slug(self):
-		mod = Module(name="Testing Module")
-		mod.save()
-		self.assertEqual(mod.slug, "testing-module")
-		self.assertEqual(mod.id, 1)
+	def test_id_slug(self):
+		mod1 = Module(name="Testing Module")
+		mod1.save()
+		mod2 = Module(name="TestingModule")
+		mod2.save()
+		self.assertEqual(mod1.slug, "testing-module")
+		self.assertEqual(mod1.id, 1)
+		self.assertEqual(mod2.slug, "testingmodule")
+		self.assertEqual(mod2.id, 2)
+
+class NoteModelTests(TestCase):
+	def notetest(self):
+		self.user = User.objects.create_user(username='testuser', password='123456')
+		login = self.client.login(username='testuser', password='123456')
+		print(login)
+		self.assertEqual(self.user.username, 'estuser')
