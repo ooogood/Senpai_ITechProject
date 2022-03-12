d19c83d8704718b74cb77b275df18b8a9b5dc1ec
Jidong Luo <2612850l@student.gla.ac.uk>
2022-03-12 05:32:08 +0000

    front-end modulepage 03120532

--- static/js/module_page.js
+++ static/js/module_page.js
@@ -1,38 +1,70 @@
 $(document).ready(function() {
-	$("#btn_sort_lik").click(function(){
-		$.get($(location).attr('href'),
-			{
-				sort_type: 'lik'
-			},
-			function(response) {
-				$('.content.links').html(response);
+	$("#sort").click(function(){
+		let order = $("#sort p").text();
+
+		if(order == "Most Likes"){
+
+			$("#sort p").text("Most Comments");
+			$.get($(location).attr('href'),
+				{
+					sort_type: 'cmt'
+				},
+				function(response) {
+					$('.content.links').html(response);
+			});
+
+		}else if(order == "Most Comments"){
+
+			$("#sort p").text("Newest");
+			$.get($(location).attr('href'),
+				{
+					sort_type: 'new'
+				},
+				function(response) {
+					$('.content.links').html(response);
 			})
+
+		}else if(order == "Newest"){
+
+			$("#sort p").text("Oldest");
+			$.get($(location).attr('href'),
+				{
+					sort_type: 'old'
+				},
+				function(response) {
+					$('.content.links').html(response);
+			})
+
+		}else if(order == "Oldest"){
+
+			$("#sort p").text("Most Likes");
+			$.get($(location).attr('href'),
+				{
+					sort_type: 'lik'
+				},
+				function(response) {
+					$('.content.links').html(response);
+			})
+
+		}
 	});
-	$("#btn_sort_cmt").click(function(){
-		$.get($(location).attr('href'),
-			{
-				sort_type: 'cmt'
-			},
-			function(response) {
-				$('.content.links').html(response);
-			})
+
+	// scroll beautify
+	$('.moduleframe').niceScroll({
+		cursorcolor: 'white',
+		cursorwidth: '12px',
+		cursorborder:'none',
+		autohidemode: 'leave',
+		horizrailenabled:false,
+		railpadding: { top: 96, right: 0, left: 0, bottom: 12 }
 	});
-	$("#btn_sort_new").click(function(){
-		$.get($(location).attr('href'),
-			{
-				sort_type: 'new'
-			},
-			function(response) {
-				$('.content.links').html(response);
-			})
-	});
-	$("#btn_sort_old").click(function(){
-		$.get($(location).attr('href'),
-			{
-				sort_type: 'old'
-			},
-			function(response) {
-				$('.content.links').html(response);
-			})
+
+	$('.noteframe').niceScroll({
+		cursorcolor: 'white',
+		cursorwidth: '12px',
+		cursorborder:'none',
+		autohidemode: 'leave',
+		horizrailenabled:false,
+		railpadding: { top: 96, right: 0, left: 0, bottom: 12 }
 	});
 });
