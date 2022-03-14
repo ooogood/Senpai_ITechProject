47b5799e7f729768b0f241eabd82550da02a8f9a
Jidong Luo <2612850l@student.gla.ac.uk>
2022-03-14 04:00:53 +0000

    front-end mylike 03140400

---
+++ static/js/mylike_page.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	// scroll beautify
+	$('.likesframe').niceScroll({
+		cursorcolor: 'white',
+		cursorwidth: '12px',
+		cursorborder:'none',
+		autohidemode: 'leave',
+		horizrailenabled: false,
+		railpadding: { top: 96, right: 0, left: 0, bottom: 12 }
+	});
+});
