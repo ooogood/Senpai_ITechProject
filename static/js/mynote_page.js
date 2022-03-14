d3afb327567b408dbdb190ecd88862018ef54f4c
Jidong Luo <2612850l@student.gla.ac.uk>
2022-03-14 00:26:38 +0000

    front-end mynote 03140026

--- static/js/mynote_page.js
+++ static/js/mynote_page.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-	$(".notelist").on('click',"button.delete_note",function(){
+	$(".notesframe").on('click',".delete_note",function(){
 		var note_id = $(this).data("noteid");
 		$.get($(location).attr('href'),
 			{
 				'noteid': note_id
 			},
 			function(response) {
-				$('.notelist').html(response);
+				$('.notesframe').html(response);
 			})
+	});
+
+	// scroll beautify
+	$('.notesframe').niceScroll({
+		cursorcolor: 'white',
+		cursorwidth: '12px',
+		cursorborder:'none',
+		autohidemode: 'leave',
+		horizrailenabled: false,
+		railpadding: { top: 96, right: 0, left: 0, bottom: 12 }
 	});
 });
