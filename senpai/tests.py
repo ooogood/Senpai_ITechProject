6a4349c7a19b75d152ebd58ba221b905d88b450f
Jin Wang <2612671w@student.gla.ac.uk>
2022-03-18 21:13:37 +0000

    test_commit

--- senpai/tests.py
+++ senpai/tests.py
@@ -31,46 +31,58 @@ class ModelTests(TestCase):
 class PageAvailabilityTest(TestCase):
 	#test user can get access to home
 	def test_home(self):
-		u = add_user('TestUser')
-		u.set_password('1')
+		
+		#if not logged in, user would be redirect
 		response = self.client.get(reverse('senpai:home'))
 		self.assertEqual(response.status_code,302)
+		
+		#else, they can get access to home
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
 		response = self.client.get(reverse('senpai:home'))
 		self.assertEqual(response.status_code,200)
 		
+	#test user can get access to module page
 	def test_module(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
 		mod1 = add_module('Testing Module')
+		
 		response = self.client.get(reverse('senpai:show_module',kwargs={'module_name_slug':mod1.slug}))
 		self.assertEqual(response.status_code,200)
-		
+	
+	#test user can get access to note page	
 	def test_note(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
 		mod1 = add_module('Testing Module')
 		note1 = add_note(mod1,u,'testnote','example_note.pdf')
 		url = reverse('senpai:show_note',kwargs={'note_id':note1.id})
+		
 		response = self.client.get(url)
 		self.assertEqual(response.status_code,200)
-		
+	
+	#test user can get access to mynote page	
 	def test_mynote(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
+		
 		response = self.client.get(reverse('senpai:mynote'))
 		self.assertEqual(response.status_code,200)
-		
+	
+	#test user can get access to mymodule page		
 	def test_mymodule(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
+		
 		response = self.client.get(reverse('senpai:mymodule'))
 		self.assertEqual(response.status_code,200)
-		
+	
+	#test user can get access to mylike page
 	def test_mylike(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
+		
 		response = self.client.get(reverse('senpai:mylike'))
 		self.assertEqual(response.status_code,200)
 
@@ -101,7 +113,7 @@ class PageAvailabilityTest(TestCase):
 		self.assertEqual(response.status_code,200)
 
 class PageUseAblilityTest(TestCase):
-	
+	# test home page has module list
 	def test_homepage_module_appears(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
@@ -109,22 +121,26 @@ class PageUseAblilityTest(TestCase):
 		mod2 = add_module('Testing Module2')
 		add_enrollment(mod2,u)
 		
+		# both module should appears, mod1 is in other_modules, mod2 is in my_modules
 		response = self.client.get(reverse('senpai:home'))
 		self.assertContains(response,mod1.name)
 		self.assertContains(response,mod2.name)
 		self.assertIn(mod1,response.context['other_modules'])
 		self.assertIn(mod2,response.context['my_modules'])
 		
+	# test module page has note list	
 	def test_module_page_note_appears(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
 		mod1 = add_module('Testing Module')
 		n = add_note(mod1,u,'test_note','example_note.pdf')
 		
+		# context should have note object and the title of the note should on the page.
 		response = self.client.get(reverse('senpai:show_module',kwargs={'module_name_slug':mod1.slug}))
 		self.assertContains(response,n.title)
 		self.assertIn(n,response.context['notes'])
-		
+	
+	# test note page can see comments and likes		
 	def test_notepage_comments_and_likes(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
@@ -133,6 +149,7 @@ class PageUseAblilityTest(TestCase):
 		Comment = 'Good note!'
 		response = self.client.get(reverse('senpai:show_note',kwargs={'note_id':n.id}))
 		
+		# user has not like or comment this note 
 		self.assertEqual(response.context['likes'],0)
 		self.assertEqual(response.context['liked'],0)
 		self.assertNotContains(response,Comment)
@@ -140,21 +157,28 @@ class PageUseAblilityTest(TestCase):
 		add_like(u, n)
 		add_comment(n,u,Comment)
 		
+		# user has liked and commented this note 
 		response = self.client.get(reverse('senpai:show_note',kwargs={'note_id':n.id}))
 		self.assertEqual(response.context['likes'],1)
 		self.assertEqual(response.context['liked'],1)
 		self.assertContains(response,Comment)
-		
+	
+	# test mynote page has user's note and do not has other's note
 	def test_my_note_note_list(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
+		u2 = add_user('Dio')
 		mod1 = add_module('Testing Module')
-		n = add_note(mod1,u,'test_note','example_note.pdf')
+		n = add_note(mod1,u,'test_note1','example_note.pdf')
+		n2 = add_note(mod1,u2,'test_note2','example_note.pdf')
 		
 		response = self.client.get(reverse('senpai:mynote'))
 		self.assertContains(response,n.title)
+		self.assertNotContains(response,n2.title)
 		self.assertIn(n,response.context['notes'])
+		self.assertNotIn(n2,response.context['notes'])
 		
+	# test mymodule page has all modules,  mod1 is in other_modules, mod2 is in my_modules
 	def test_my_module_module_list(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
@@ -167,45 +191,61 @@ class PageUseAblilityTest(TestCase):
 		self.assertContains(response,mod2.name)
 		self.assertIn(mod1,response.context['other_modules'])
 		self.assertIn(mod2,response.context['user_modules'])
-		
+	
+	# test mylike page has liked notes and no unliked notes
 	def test_my_like_liked_note_list(self):
 		u = add_user('JoJo')
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
 		mod1 = add_module('Testing Module')
 		n = add_note(mod1,u,'test_note','example_note.pdf')
 		
+		# User has not liked note, note should not in this page
 		response = self.client.get(reverse('senpai:mylike'))
 		self.assertNotContains(response,n.title)
 		
 		like = add_like(u,n)
 		
+		# User liked note, note should in this page
 		response = self.client.get(reverse('senpai:mylike'))
 		self.assertContains(response,n.title)
-		
+	
+	# test sign in, sign up, and logout functions.
 	def test_sign_inup_and_logout(self):
+		# Users has not logged in should be redirect
+		response = self.client.get(reverse('senpai:home'))
+		self.assertEqual(response.status_code,302)
+		
+		# A signed-up user should be auto logged in
 		self.client.post(reverse('senpai:signinup'),{'username': 'admintest', 'password':'1', 'email':'sb@1.com', 'signup_form':'submit'})
 		response = self.client.get(reverse('senpai:home'))
 		self.assertEqual(response.status_code,200)
 		
+		# If user logged out, he can not get access to home page
 		self.client.get(reverse('senpai:logout'))
 		response = self.client.get(reverse('senpai:home'))
 		self.assertEqual(response.status_code,302)
 		
+		# And if he logged in, he can get access to home page
 		self.client.post(reverse('senpai:signinup'),{'username': 'admintest', 'password':'1', 'signin_form':'submit'})
 		response = self.client.get(reverse('senpai:home'))
 		self.assertEqual(response.status_code,200)
 	
+	# test if an admin can signed with admin-key.
 	def test_admin_signup(self):
 		u = add_user('JoJo',1)
 		up = UserProfile.objects.get(user=u)
 		self.client.post(reverse('senpai:signinup'),{'username': 'admintest', 'password':'1', 'email':'sb@1.com', 'admin_key':up.admin_key, 'signup_form':'submit'})
+		
+		# signed up successful
 		response = self.client.get(reverse('senpai:home'))
 		self.assertEqual(response.status_code,200)
 		
+		# check admin status
 		admin = User.objects.get(username = 'admintest')
 		adminp = UserProfile.objects.get(user=admin)
 		self.assertEqual(adminp.is_admin,1)
-		
+	
+	# test any module added is in module management page
 	def test_module_management_module_list(self):
 		u = add_user('JoJo',1)
 		self.client.login(username='JoJo', password='JoJoisnumber1!')
